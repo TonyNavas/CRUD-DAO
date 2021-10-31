393544f080e7cfeaff096268a91472f69164a50e
TonyNavas <tonynavas777@gmail.com>
2021-10-31 11:29:46 -0600

    Termine la conexion

--- src/main/java/modelo/Conexion.java
+++ src/main/java/modelo/Conexion.java
@@ -1,8 +1,8 @@
 package modelo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexion {
     private Connection conexion;
@@ -12,8 +12,22 @@ public class Conexion {
         try {
             Class.forName("org.postgresql.Driver");
             conexion = DriverManager.getConnection("rjdbc:postgresql://localhost:5432/PEDIDOS","postgres", "123");
+            st = conexion.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    public ResultSet CONSULTAR(String sql) throws SQLException {
+        return st.executeQuery(sql);
+    }
+    public int GUARDAR(String sql) throws  SQLException{
+        return st.executeUpdate(sql);
+    }
+    public void CERRAR(String sql){
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
